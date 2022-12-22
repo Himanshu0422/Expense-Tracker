90e4f132c801682731f3f930d186c45d93740e7f
Himanshu0422 <himanshumittal035@gmail.com>
2022-12-22 21:40:03 +0530

    add expense form

---
+++ src/components/Newexpense/Expenseform.js
@@ -0,0 +1,95 @@
+import React from "react";
+import './Expenseform.css';
+
+export default function Expenseform(props) {
+
+    // const [enteredTitle, setEnteredTitle] = React.useState('');
+    // const [enteredAmout, setEnteredAmount] = React.useState('');
+    // const [enteredDate, setEnteredDate] = React.useState('');
+
+    const [enteredData, setEnteredData] = React.useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+
+    const titleChange = (event) => {
+        setEnteredData((prevstate) => {
+            return {
+                ...prevstate,
+                enteredTitle: event.target.value
+            };
+        });
+    };
+    const amountChange = (event) => {
+        setEnteredData((prevstate) => {
+            return {
+                ...prevstate,
+                enteredAmount: event.target.value
+            };
+        });
+    }
+    const dateChange = (event) => {
+        setEnteredData((prevstate) => {
+            return {
+                ...prevstate,
+                enteredDate: event.target.value
+            };
+        });
+    }
+
+    const submithandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredData.enteredTitle,
+            amount: enteredData.enteredAmount,
+            date: new Date(enteredData.enteredDate)
+        };
+        // console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+        setEnteredData({
+            enteredTitle: '',
+            enteredAmount: '',
+            enteredDate: ''
+        })
+    };
+
+    return (
+        <form onSubmit={submithandler}>
+            <div className="new-expense_controls">
+                <div className="new-expense_control">
+                    <label>Title</label>
+                    <input
+                        type='text'
+                        onChange={titleChange}
+                        value={enteredData.enteredTitle}
+                    />
+                </div>
+                <div className="new-expense_control">
+                    <label>Amount</label>
+                    <input
+                        type='number'
+                        min="1"
+                        step="0.1"
+                        onChange={amountChange}
+                        value={enteredData.enteredAmount}
+                    />
+                </div>
+                <div className="new-expense_control">
+                    <label>Date</label>
+                    <input
+                        type='date'
+                        min="2020-01-01"
+                        max="2022-12-31"
+                        onChange={dateChange}
+                        value={enteredData.enteredDate}
+                    />
+                </div>
+            </div>
+            <div className="new-expense_actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
