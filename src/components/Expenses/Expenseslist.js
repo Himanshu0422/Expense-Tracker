b03fc3c43318223ec926ca9c56702dbd0809ea78
Himanshu0422 <himanshumittal035@gmail.com>
2022-12-26 22:01:08 +0530

    expenselist

---
+++ src/components/Expenses/Expenseslist.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './Expenseslist.css'
+
+export default function Expenselist(props) {
+    // const hlo = props.items.length === 0 ?
+    //     (<p>No Expense</p>) :
+
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list__fallback">Found No expenses.</h2>
+    }
+
+    return (
+        <ul className="expenses-list">
+            {props.items.map(expense => {
+                return (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                )
+            })}
+        </ul>
+    )
+}
