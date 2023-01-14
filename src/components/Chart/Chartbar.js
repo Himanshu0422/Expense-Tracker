d1c5f9f9c8f990595ecb0d29bfcc4dc04a4826dd
Himanshu0422 <himanshumittal035@gmail.com>
2023-01-14 16:36:19 +0530

    fixed some errors

--- src/components/Chart/Chartbar.js
+++ src/components/Chart/Chartbar.js
@@ -4,19 +4,19 @@ import './Chartbar.css';
 const Chartbar = props => {
 
     let barheight = '0%';
-    if(props.maxValue > 0){
-        barheight = Math.round((props.value/ props.maxValue) * 100) + '%';
+    if (props.maxValue > 0) {
+        barheight = Math.round((props.value / props.maxValue) * 100) + '%';
     }
 
-    return(
+    return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="chart-bar__fill" style={{height: barheight}}>
+                <div className="chart-bar__fill" style={{ height: barheight }}>
 
                 </div>
-                <div className="chart-bar__label">
-                    {props.lebel}
-                </div>
+            </div>
+            <div className="chart-bar__label">
+                {props.label}
             </div>
         </div>
     )
