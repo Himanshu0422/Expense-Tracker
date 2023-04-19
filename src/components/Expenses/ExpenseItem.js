fe95e984d3436e61157a4efaf4361b5b04963bf9
Himanshu0422 <himanshumittal035@gmail.com>
2023-04-19 20:01:10 +0530

    added context in expenseitem file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
+import axios from 'axios'
+import expenseContext from "../../store/expenseContext";
 
 export default function ExpenseItem(props) {
-
-    const [editMode,setEditMode] = useState(false)
-    const [title,setTitle] = useState(props.title)
-    const [amount,setAmount] = useState(props.amount)
+    const expensectx = useContext(expenseContext);
+    const [editMode,setEditMode] = useState(false);
+    const [title,setTitle] = useState(props.title);
+    const [amount,setAmount] = useState(props.amount);
+    const [updatedDate, setDate] = useState(props.date);
 
     function handleEdit(){
-        setEditMode(true)
+        setEditMode(true);
     }
     function handleTitle(e){
-        setTitle(e.target.value)
+        setTitle(e.target.value);
     }
     function handleAmount(e){
-        setAmount(e.target.value)
+        setAmount(e.target.value);
     }
+    function handleDate(e){
+        setDate(e.target.value);
+    }
+    function handleSave(){
+        setEditMode(false);
+        editBase();
+    }
+
+    async function editBase(){
+        const updateValues = {
+            ...props,
+            title:title,
+            amount:amount,
+            date: updatedDate
+        }
+        await axios.put(`/data/${props.id}`, updateValues);
+        expensectx.fetchExpense()
+    }
+
     return (
         <li>
             <Card className='expense-item'>
-                <ExpenseDate date= {props.date}/>
+            {!editMode ? <ExpenseDate date= {new Date(updatedDate)}/> : <input value={updatedDate} onChange={handleDate} type='date'/>}
                 <div className='expense-item_descrip'>
                     <h2>{!editMode? title: <input value={title} onChange={handleTitle} type='text' />}</h2>
                     <div className='expense-item_price'>{!editMode? amount : <input value={amount} onChange={handleAmount} type='number' />}</div>
                 </div>
-                <button className='edit-button' onClick={() => {props.set(props.id)}}>Delete</button>
-                {!editMode? <button className='edit-button' onClick={handleEdit}>Edit</button>:<button className='edit-button' onClick={()=>setEditMode(false)}>Save</button>}
+                <button className='edit-button' onClick={() => {expensectx.deleteExp(props.id)}}>Delete</button>
+                {!editMode? <button className='edit-button' onClick={handleEdit}>Edit</button>:<button className='edit-button' onClick={handleSave}>Save</button>}
             </Card>
         </li>
     );
